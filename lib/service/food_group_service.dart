6196c73339ce7ca49aead3975f70c2f289e27e32
Danilo Ribeiro <daniloleiter@hotmail.com>
2025-03-18 15:04:53 -0300

    insere comidas

--- lib/service/food_group_service.dart
+++ lib/service/food_group_service.dart
@@ -60,11 +60,31 @@ class FoodGroupService {
   Future<List<FoodGroup>> _getInitialFoodGroupList() async {
       var foodList = await _foodService.findAll();
       List<FoodGroup> fgList = [];
-      var arroz = foodList.where((food) => food.name == 'Arroz').first;
-      var feijao = foodList.where((food) => food.name == 'Feijão').first;
-      var frango = foodList.where((food) => food.name == 'Peito de frango').first;
-      fgList.add(FoodGroup(name: 'Almoço', foodQuantityList: [FoodQuantity(arroz, 150), FoodQuantity(feijao, 150), FoodQuantity(frango, 200)]));
+      var whey = foodList.where((food) => food.name == 'Whey protein').first;
+      var aveia = foodList.where((food) => food.name == 'Aveia, flocos, crua').first;
+      var morango = foodList.where((food) => food.name == 'Morango, cru').first;
+      var granola = foodList.where((food) => food.name == 'Granola Light Tia Sônia').first;
+
+      fgList.add(FoodGroup(name: 'Shake com aveia', foodQuantityList: [FoodQuantity(whey, 30), FoodQuantity(aveia, 50), FoodQuantity(morango, 100),
+        FoodQuantity(granola, 15)]));
+      fgList.add(FoodGroup(name: 'Shake sem aveia', foodQuantityList: [FoodQuantity(whey, 30),  FoodQuantity(morango, 150)]));
+
+      var arroz = foodList.where((food) => food.name == 'Arroz, integral, cozido').first;
+      var feijao = foodList.where((food) => food.name == 'Feijão, carioca, cozido').first;
+      var frango = foodList.where((food) => food.name == 'Frango, peito, sem pele, grelhado').first;
+
+      fgList.add(FoodGroup(name: 'Almoço', foodQuantityList: [FoodQuantity(arroz, 150), FoodQuantity(feijao, 150), FoodQuantity(frango, 150)]));
       fgList.add(FoodGroup(name: 'Jantar', foodQuantityList: [FoodQuantity(arroz, 150), FoodQuantity(feijao, 150), FoodQuantity(frango, 150)]));
+
+      var pao = foodList.where((food) => food.name == 'Pão, trigo, forma, integral').first;
+      var cremeRicota = foodList.where((food) => food.name == 'Creme de ricota').first;
+      var cenoura = foodList.where((food) => food.name == 'Cenoura, crua').first;
+
+      fgList.add(FoodGroup(name: 'Sanduíche duplo', foodQuantityList: [FoodQuantity(pao, 50), FoodQuantity(frango, 70), FoodQuantity(cenoura, 50),
+        FoodQuantity(cremeRicota, 20)]));
+      fgList.add(FoodGroup(name: 'Sanduíche triplo', foodQuantityList: [FoodQuantity(pao, 75), FoodQuantity(frango, 100), FoodQuantity(cenoura, 70),
+        FoodQuantity(cremeRicota, 40)]));
+
       return fgList;
   }
 }
